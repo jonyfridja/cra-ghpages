d2f2438240c9fad1138f910e95f5da7f0cb9cbe6
Jonathan Fridja <jonyfridja@gmail.com>
2020-04-20 18:45:58 +0300

    Updates

---
+++ precache-manifest.5d4ea37b64bcdeccad54113b46f9b748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3711196f331da106df10f653af94abbe",
+    "url": "/cra-ghpages/index.html"
+  },
+  {
+    "revision": "673c1b56a3ccce7c1e3a",
+    "url": "/cra-ghpages/static/css/main.b8b4908a.chunk.css"
+  },
+  {
+    "revision": "c4e7cd5fc7f850594b49",
+    "url": "/cra-ghpages/static/js/2.cab0f094.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-ghpages/static/js/2.cab0f094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673c1b56a3ccce7c1e3a",
+    "url": "/cra-ghpages/static/js/main.f842e554.chunk.js"
+  },
+  {
+    "revision": "5a7e367650fd6295a24c",
+    "url": "/cra-ghpages/static/js/runtime-main.bc7a7489.js"
+  }
+]);
